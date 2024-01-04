eb13bae68e873d4df0be33efea1526919e1af78a
neeraj-gs <gsneeraj05@gmail.com>
2024-01-04 11:40:46 +0530

    Added a basic frontend page and also added images to align them

--- revivify/pages/index.tsx
+++ revivify/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,11 +23,26 @@ export default function Home() {
       Have Old Blurry Images, Want to relive and regenerate your Old memoeries? Let Our AI Revive those memories.
     </p>
 
-    <div>
-      <a href="">
+    <div className='flex justify-center space-x-4'>
+      <a href="" className='bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border'>
         Learn How Its Built 
       </a>
-      <Link href="/">Revive Your Images</Link>
+      <Link href="/" className='bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80 border'>Revive Your Images</Link>
+    </div>
+
+    <div className='flex justify-between items-center w-full flex-col sm:mt-10 mt-6'>
+      <div className='flex flex-col space-y-10 mt-4 mb-16'>
+        <div className='flex items-center sm:space-x-2 sm:flex-row flex-col'>
+          <div>
+            <h2 className='mb-1 font-medium text-lg'>Original Image</h2>
+            <Image src="" alt='original Image' className='w-96 h-96 rounded-xl' width={400} height={400}/>
+          </div>
+          <div>
+            <h2 className='mb-1 font-medium text-lg'>Revived image</h2>
+            <Image src="" alt='revived image' width={400} height={400} className='w-96 h-96 rounded-xl' />
+          </div>
+        </div>
+      </div>
     </div>
 
     </main>
