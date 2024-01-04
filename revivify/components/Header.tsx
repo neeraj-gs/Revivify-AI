5b8c7f7486d532bd6a904d39a4b9ac0b6d5d0aab
neeraj-gs <gsneeraj05@gmail.com>
2024-01-04 11:20:51 +0530

    Started stylign header componet

--- revivify/components/Header.tsx
+++ revivify/components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
-type Props = {}
-
-const Header = (props: Props) => {
+const Header = ({photo}: {photo?:string|undefined}) => {
   return (
-    <div>Header</div>
+    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+        <Link href="/">
+            <Image src="" alt="Logo" />
+            <h1>Revivify-AI</h1>
+        </Link>
+    </header>
   )
 }
 
