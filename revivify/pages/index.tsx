5ce7694f6c0fbaf99706df9fe60cd9c11c7c5e4a
neeraj-gs <gsneeraj05@gmail.com>
2024-01-04 11:30:14 +0530

    Added a basic front apge

--- revivify/pages/index.tsx
+++ revivify/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,16 +12,22 @@ export default function Home() {
         <title>Revivify-AI</title>
       </Head>
       <Header />
-      <main className="flex flex-col flex-1">
+      <main className="flex flex-col flex-1 w-full items-center justify-center text-center px-4 sm:mt-28 mt-20">
       <h1 className='MX-AUTO max-w-4xl text-5xl font-bold tracking-normal text-slate-900 sm:text-6xl'>Revive Your Old Blurry Images{" "} Using Our {" "}
       <span className='relative whitespace-nowrap text-[#3290EE]'>
         AI.
       </span>
     </h1>
     <p className='mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7'>
-      Have Old Blurry Images, Want to relive and regenerate your Old memoeries? Let Our AI Revive those memeories and love on.
-      <br /> 100% Free - Revive Your Images Now.
+      Have Old Blurry Images, Want to relive and regenerate your Old memoeries? Let Our AI Revive those memories.
     </p>
+
+    <div>
+      <a href="">
+        Learn How Its Built 
+      </a>
+      <Link href="/">Revive Your Images</Link>
+    </div>
 
     </main>
     </div>
