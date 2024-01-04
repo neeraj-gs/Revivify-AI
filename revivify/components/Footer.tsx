01117aa1caf7e92bbbf56c76bfa3d74c9dbe566b
neeraj-gs <gsneeraj05@gmail.com>
2024-01-04 11:52:24 +0530

    Added a basic footer section layout need to style it for better visibility

--- revivify/components/Footer.tsx
+++ revivify/components/Footer.tsx
@@ -1,16 +1,23 @@
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer>
-        <div>
+    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+        <div className="">
             Powered by{" "}
-            <a href="">
+            <a href="" className="font-bold transition hover:text-black/50">
                 Replicate AI,{" "}
             </a>
-            <a href="">Upload,{" "}</a>
-            <a href="">Next.JS,{" "}</a>
+            <a href="" className="font-bold transition hover:text-black/50">Upload,{" "}</a>
+            <a href="" className="font-bold transition hover:text-black/50">Next.JS,{" "}</a>
             and{" "}
-            <a href="">Vercel{" "}</a>
+            <a href="" className="font-bold transition hover:text-black/50">Vercel{" "}</a>
+        </div>
+
+        <div className="flex items-center gap-1">
+            <Link href="/">Download Source Code</Link>{" | "}
+            <p>Created With Love By Neeraj GS</p>
+
         </div>
     </footer>
   )
